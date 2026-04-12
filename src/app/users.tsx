37960d38f0727a75b289651d632fe24bfec20deb
Aoarna1-ag <aparnagupta.csa@gmail.com>
2026-04-12 21:46:48 +0530

    fetching users from backend stored in aws db

---
+++ src/app/users.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text } from "react-native";
+// import "./app.css";
+
+type Users = {
+  username: string;
+  id: number;
+  email: string;
+};
+
+const Users = () => {
+  const [usersList, setUsersList] = useState<Users[]>([]);
+
+  const url = `http://localhost:3001/list`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.items);
+        setUsersList(data.items);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <ScrollView>
+      {usersList.map((item, i) => {
+        return (
+          <View
+            key={i}
+            className="user"
+            style={{
+              height: 48,
+              backgroundColor: "pink",
+              width: "100%",
+              borderColor: "#fff",
+              borderWidth: 1,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Text> {item.username}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+export default Users;
